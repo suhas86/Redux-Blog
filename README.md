4242e03998fc8dbc80f86a59eee035795dd104e8
SUHAS <suhas.m@bvbi-infotech.com>
2017-07-18 23:30:21 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# ReduxBlogSample
+
+
+
+### Getting Started
+
+There are two methods for getting started with this repo.
+
+#### Familiar with Git?
+Checkout this repo, install dependencies, then start the gulp process with the following:
+
+```
+> git clone https://github.com/suhas86/Redux-Blog.git
+> cd redux-blog
+> npm install
+> npm start
+```
+
+#### Not Familiar with Git?
+Click [here](https://github.com/suhas86/Redux-Blog) then download the .zip file.  Extract the contents of the zip file, then open your terminal, change to the project directory, and:
+
+```
+> npm install
+> npm start
+```
---
+++ src/components/post_index.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
+import _ from 'lodash';
+
+class PostIndex extends Component {
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+    renderPost() {
+        return _.map(this.props.posts, post => {
+            return (
+                
+             <li className="list-group-item" key={post.id}>
+                 {post.title}
+             </li> 
+        )
+        });
+
+    }
+    render() {
+
+        return (
+            <div>
+                <h3>Posts</h3>
+                <ul className="list-group">
+                    {this.renderPost()}
+                </ul>
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    return { posts: state.posts }
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostIndex);
